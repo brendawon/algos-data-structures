b218526e5439cf5ba3dd80df7a3d6b6a862364d3
Brenda Wong <Brendot.wong@gmail.com>
2021-09-20 00:03:39 -0400

    needleHaystack fix

--- Topics/Pointers/needleHaystack.js
+++ Topics/Pointers/needleHaystack.js
@@ -5,26 +5,27 @@ var strStr = function (haystack, needle) {
   // edge case: if needle.length is < 1, return 0
   if (needle.length < 1) return 0;
 
-  // if needle.length <= haystack
-  if (needle.length <= haystack.length) {
-    //while loop through haystack to find a match with needle
-    while (matchingChars < needle.length || hI < haystack.length) {
-      //when there is a match, increment match
-      if (needle[matchingChars] === haystack[hI]) {
-        matchingChars++;
-      } else {
-        if (matchingChars > 0) {
-          //when not a match, reset h1 to point of first match (hI - matchingChars)
-          hI = hI - matchingChars;
-          matchingChars = 0;
-        }
+  //while loop through haystack to find a match with needle
+  while (matchingChars < needle.length && hI < haystack.length) {
+    //when there is a match, increment match
+    if (needle[matchingChars] === haystack[hI]) {
+      matchingChars++;
+    } else {
+      if (matchingChars > 0) {
+        //when not a match, reset h1 to point of first match (hI - matchingChars) and set match to 0
+        hI = hI - matchingChars;
+        matchingChars = 0;
       }
-      hI++;
     }
+    //increment h index to move along the haystack
+    hI++;
   }
+
+  //after loop, check if matching characters = needle length, if it does, subtract the needle from the recorded h index
   if (matchingChars === needle.length) {
     return hI - needle.length;
   } else {
+    //if not equal, return -1
     return -1;
   }
 };
