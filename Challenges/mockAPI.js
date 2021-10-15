e2f79bfc86674a99346f1902eb00cb5715bdea2d
Brenda Wong <Brendot.wong@gmail.com>
2021-10-14 23:05:13 -0400

    pet challenges

---
+++ Challenges/mockAPI.js
@@ -0,0 +1,3 @@
+//mock api call to retrieve info in object and return in array
+//[{pet: type, name: nameOfPet}, {pet:type, name: nameOfPet}] and [{pet: type, food: foodType}, {pet: type, food: foodType}]
+//return an array that combines the items of the same type in one object: [{pet: type, name: nameOfPet, food: foodType}]
