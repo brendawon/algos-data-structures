7100fd0aac31448dd85fcd0612baecdbd3f131f8
Brenda Wong <Brendot.wong@gmail.com>
2021-10-19 13:28:40 -0400

    simpler majorityElement

--- Topics/Frequency/majorityElement.js
+++ Topics/Frequency/majorityElement.js
@@ -10,15 +10,13 @@ var majorityElement = function (nums) {
     }
   }
   //find the largest value in the obj
-  let values = Object.values(obj);
   let majority = 0;
-  let index;
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] > majority) {
-      majority = values[i];
-      index = i;
+  let result;
+  for (let key in obj) {
+    if (obj[key] > majority) {
+      majority = obj[key];
+      result = key;
     }
   }
-  //check the object key at that index
-  return Object.keys(obj)[index];
+  return result;
 };
