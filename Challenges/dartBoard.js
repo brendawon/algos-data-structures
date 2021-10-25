e2ef661541d9db956ad98c556cb0e6e6cbbba98f
Brenda Wong <Brendot.wong@gmail.com>
2021-10-24 21:20:18 -0400

    dartBoard

---
+++ Challenges/dartBoard.js
@@ -0,0 +1,6 @@
+//calculate the score on a dart board
+//20 number values on the dart board
+//bull's eye, bull, double inner circle, double outer circle, triple inner circle, triple outer circle
+//use of pythagorean theorem - hypotenuse = radius (dart's location)
+//use of atan2(y,x) to find the radian, use of radian to degrees
+//find degree boundaries for number value scores
