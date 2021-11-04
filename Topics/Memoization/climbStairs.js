3bd94bbdbffd3c2143a1f09d2b43f6dc5355f2f6
Brenda Wong <Brendot.wong@gmail.com>
2021-11-04 19:44:46 -0400

    fib with constant space

--- Topics/Memoization/climbStairs.js
+++ Topics/Memoization/climbStairs.js
@@ -21,3 +21,24 @@ var climbStairs = function (n, memo = {}) {
   }
   return memo[n];
 };
+
+/*
+O(n) time and O(1) space
+function getNthFib(n){
+  //last two stores the last two values in the fib sequence
+  const lastTwo = [0,1];
+  let counter = 3;
+  while(counter <= n){
+    //next fib value of counter = the sum of the lastTwo
+
+    const nextFib = lastTwo[0] + lastTwo[1];
+    //swap the values to store next fib now as one of the last two fib values and remove the 'oldest' value
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextFib;
+    //counter increments and loop continues until we reach the nth fib
+    counter++;
+  }
+  //when n is > 1 we return the second stored value, otherwise we return the first stored value
+  return n > 1 ? lastTwo[1] : lastTwo[0];
+}
+*/
